136a2bb08877cf621c0d42d5085144c044065b4f
ross-brown <rbrown_98@yahoo.com>
2023-05-17 14:58:18 -0500

    add functionality to change grid size

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const size = 16;
+const SIZE = 16;
 
 const grid = document.querySelector(".grid");
+const clearBtn = document.querySelector("#clear");
+const changeBtn = document.querySelector("#change-size");
 
 function createGrid(size) {
-  for (let i = 1; i <= size ** 2; i++) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 1; i <= size * size; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-square");
     grid.appendChild(gridElement);
@@ -16,6 +21,25 @@ function onMouseHover(e) {
   }
 }
 
-grid.addEventListener("mouseover", onMouseHover);
+function clearGrid() {
+  for (let i = 0; i < grid.children.length; i++) {
+    const currentSquare = grid.children[i];
+    currentSquare.style.backgroundColor = "white";
+  }
+}
 
-createGrid(size);
+function changeSize() {
+  const newSize = prompt("How many squares per side would you like?");
+  if (+newSize > 100) {
+    alert('Cannot exceed 100 x 100')
+  } else {
+    clearGrid();
+    createGrid(+newSize);
+  }
+}
+
+grid.addEventListener("mouseover", onMouseHover);
+clearBtn.addEventListener("click", clearGrid);
+changeBtn.addEventListener("click", changeSize);
+
+createGrid(SIZE);
